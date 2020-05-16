da0f4957306e296ff0d991f86fb557f283fe4338
Roman-Zvizlo <armwrestler241@gmail.com>
2020-05-16 22:14:38 +0000

    ready

--- js/product-list.js
+++ js/product-list.js
@@ -12,7 +12,7 @@ class ProductList {
     let productListDomString = '';
     const products = await this.productService.getProducts();
     products.forEach(product => {
-      productListDomString += `<div class="col-12 col-sm-6 col-md-4 col-lg-3 mb-3">
+      productListDomString += `<div class="col-12 col-sm-6 col-md-4 col-lg-4">
                   <div class="card product">
                     <img class="card-img-top" src="img/products/${product.image}" 
                         alt="${product.title}">
@@ -21,10 +21,10 @@ class ProductList {
                       <p class="card-text flex-fill">${product.description}</p>
                       <div class="d-flex justify-content-around">
                         <button class="btn btn-info" data-toggle="modal"
-                          data-target="#productInfoModal" data-id="${product.id}">Info
+                          data-target="#productInfoModal" data-id="${product.id}">Детальніше
                         </button>
                         <button class="btn btn-primary buy" data-id="${product.id}">
-                          $${product.price} - Buy
+                          ${product.price} грн - Купити
                         </button>
                       </div>
                     </div>
@@ -63,13 +63,13 @@ class ProductList {
     modal.querySelector('.modal-body .card-text').innerText =
       product.description;
     const btnBuy = modal.querySelector('button.buy');
-    btnBuy.innerText = `${product.price} - Buy`;
+    btnBuy.innerText = `${product.price} грн - Купити`;
     btnBuy.dataset.id = id;
   }
   handleProductBuyClick(event) {
     const button = event.target;
     const id = button.dataset.id;
     this.cart.addProduct(id);
-    window.showAlert('Product added to cart');
+    window.showAlert('Товар додано до корзини');
   }
 }
